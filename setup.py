11f7a377bb56dc7e0f7288dc06f964ece5ce7e66
bikesh32 <bikeshsingh023@gmail.com>
2023-05-09 20:28:06 +0530

    all file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+
+setup(name="Census-income",
+      version = "0.0.1",
+      Author = "Bikesh",
+      authormail = "bikeshsingh023@gmail.com",
+      packages = find_packages(),
+      intall_requires = ["pandas","numpy","flask"]
+    )
